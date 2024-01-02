2a3e375b7d906c3dd07438ee6d0fd23196c0e70c
migue <miguel.lau@etecsa.cu>
2024-01-02 18:25:59 -0500

    primeros cambios a la conf del torneo

--- components/Tourney/Competitors/Players.js
+++ components/Tourney/Competitors/Players.js
@@ -80,7 +80,7 @@ export default function Players({tourney}) {
         setPage(pageNumber);
     };
 
-    const removePlayer = async (id) => {
+    const changePlayer = async (id, value) => {
         const url = `${process.env.NEXT_PUBLIC_API_URL}player/${id}`
     
         try {
@@ -121,6 +121,41 @@ export default function Players({tourney}) {
               }
             }
         }
+    }
+
+    const onChange = (id, value) => {
+        let msg = "";
+        let title = "";
+
+        switch (value) {
+            case "PAUSE":
+                title="Pausar Jugador";
+                msg = "Estás seguro que desea Pausar éste jugador";
+                break;
+            case "PLAYING":
+                title="Regresar al Juego";
+                msg = "Estás seguro que desea regresar éste jugador al juego";
+                break;
+            case "EXPELLED":
+                title="Expulsar Jugador";
+                msg = "Estás seguro que desea expulsar éste jugador del juego";
+                break;
+        }
+
+        Swal.fire({
+            title: title,
+            text: msg,
+            icon: "question",
+            showCancelButton: true,
+            cancelButtonText: "Cancelar",
+            allowOutsideClick: false,
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Aceptar",
+          }).then((result) => {
+            if (result.isConfirmed) {
+                changePlayer(id, value);
+            }
+        });
     }    
 
     return (
@@ -146,9 +181,9 @@ export default function Players({tourney}) {
                         >
                             <option value={0}>Aceptados</option>
                             <option value={1}>Jugando</option>
-                            <option value={3}>Esperando</option>
-                            <option value={4}>Expulsado</option>
-                            <option value={5}>Pausa</option>
+                            <option value={2}>Esperando</option>
+                            <option value={3}>Expulsado</option>
+                            <option value={4}>Pausa</option>
                         </Input>
                     </InputGroup>
                 </div>
@@ -207,32 +242,61 @@ export default function Players({tourney}) {
                                     />
                                     <div className="d-flex flex-column flex-fill ms-2">
                                         <span className="gamer-couple">{item.name}</span>
-                                        <small>{item.profile_type}. {item.city_name + ", " + item.country}</small>
+                                        {(item.city_name !== "" && item.country) &&                                        <small>{item.city_name + ", " + item.country}</small>}
                                     </div>
 
-                                    {tourney.status === "CREATED" && (<div>
-                                        <div
-                                            className="rounded p-2 trash-effect"
-                                            title="Eliminar jugador"
-                                            onClick={(e) => {removePlayer(item.id)}}
-                                        >
-                                        <i
-                                            className="bi bi-person-dash"
-                                            style={{ fontSize: "24px" }}
-                                        ></i>
+
+                                    {(tourney.status_name === "CREATED" && item.status_name === "PLAYING") && (
+                                        <div className="d-flex">
+                                            <div
+                                                className="rounded p-1 trash-effect"
+                                                title="Pausar jugador"
+                                                onClick={(e) => {onChange(item.id, "PAUSE")}}
+                                            >
+                                                <i
+                                                    className="rounded-circle bg-success text-white bi bi-pause p-1"
+                                                    style={{ fontSize: "16px" }}
+                                                ></i>
+                                            </div>
+
+                                            <div
+                                                className="rounded p-1 trash-effect"
+                                                title="Expulsar jugador"
+                                                onClick={(e) => {onChange(item.id, "EXPELLED")}}
+                                            >
+                                                <i
+                                                    className="rounded-circle bg-danger text-white bi bi-escape p-1"
+                                                    style={{ fontSize: "16px" }}
+                                                ></i>
+                                            </div>
                                         </div>
-                                    </div>)}
-                                    {(tourney.status === "INITIADED" || tourney.status === "CONFIGURATED") && (<div>
-                                        <div
-                                            className="rounded p-2"
-                                            title="Jugador Aceptado"
-                                        >
-                                        <i
-                                            className="bi bi-patch-check"
-                                            style={{ fontSize: "24px", color: "blue" }}
-                                        ></i>
+                                    )}
+
+                                    {(tourney.status_name === "CREATED" && item.status_name === "PAUSE") && (
+                                        <div className="d-flex">
+                                            <div
+                                                className="rounded p-1 trash-effect"
+                                                title="Volver al Juego"
+                                                onClick={(e) => {onChange(item.id, "PLAYING")}}
+                                            >
+                                                <i
+                                                    className="rounded-circle bg-primary text-white bi bi-person-check p-1"
+                                                    style={{ fontSize: "16px" }}
+                                                ></i>
+                                            </div>
+                                            <div
+                                                className="rounded p-1 trash-effect"
+                                                title="Expulsar jugador"
+                                                onClick={(e) => {onChange(item.id, "EXPELLED")}}
+                                            >
+                                                <i
+                                                    className="rounded-circle bg-danger text-white bi bi-escape p-1"
+                                                    style={{ fontSize: "16px" }}
+                                                ></i>
+                                            </div>
+
                                         </div>
-                                    </div>)}
+                                    )}
 
                                 </div>
 
@@ -251,19 +315,19 @@ export default function Players({tourney}) {
                     <Empty path1="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" path2="" message="Los jugadores aceptados para participar en el torneo aparecerán aquí." />
                 </div>             
             )}
-                {totalPages > 1 && 
-                    <div className="row">
-                        <Pagination
-                            onChangePage={onChangePage}
-                            currentPage={page}
-                            totalPage={totalPages}
-                            totalCount={total}
-                            rowsPerPage={rowsPerPage}
-                            siblingCount={1}
-                            showInfo={false}
-                        />
-                    </div>          
-                }
-            </div>
+            {totalPages > 1 && 
+                <div className="row">
+                    <Pagination
+                        onChangePage={onChangePage}
+                        currentPage={page}
+                        totalPage={totalPages}
+                        totalCount={total}
+                        rowsPerPage={rowsPerPage}
+                        siblingCount={1}
+                        showInfo={false}
+                    />
+                </div>          
+            }
+        </div>
     )
 };
